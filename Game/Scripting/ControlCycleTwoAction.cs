2c4f6480c1c4e023ed20ac53dcf732422c95b41e
Cayden Lords <lor20002@byui.edu>
2022-06-21 09:36:55 -0600

    CYCLECONTROL

---
+++ Game/Scripting/ControlCycleTwoAction.cs
@@ -0,0 +1,58 @@
+using Unit05_cycle.Game.Casting;
+using Unit05_cycle.Game.Services;
+
+
+namespace Unit05_cycle.Game.Scripting
+{
+    /// <summary>
+    /// <para>An input action that controls the snake.</para>
+    /// <para>
+    /// The responsibility of ControlActorsAction is to get the direction and move the snake's head.
+    /// </para>
+    /// </summary>
+    public class ControlCycleTwoAction : Action
+    {
+        private KeyboardService keyboardService;
+        private Point direction = new Point(Constants.CELL_SIZE, 0);
+
+        /// <summary>
+        /// Constructs a new instance of ControlActorsAction using the given KeyboardService.
+        /// </summary>
+        public ControlCycleTwoAction(KeyboardService keyboardService)
+        {
+            this.keyboardService = keyboardService;
+        }
+
+        /// <inheritdoc/>
+        public void Execute(Cast cast, Script script)
+        {
+            // left
+            if (keyboardService.IsKeyDown("j"))
+            {
+                direction = new Point(-Constants.CELL_SIZE, 0);
+            }
+
+            // right
+            if (keyboardService.IsKeyDown("l"))
+            {
+                direction = new Point(Constants.CELL_SIZE, 0);
+            }
+
+            // up
+            if (keyboardService.IsKeyDown("i"))
+            {
+                direction = new Point(0, -Constants.CELL_SIZE);
+            }
+
+            // down
+            if (keyboardService.IsKeyDown("k"))
+            {
+                direction = new Point(0, Constants.CELL_SIZE);
+            }
+
+            CycleOne cycleone = (CycleOne)cast.GetFirstActor("cycleone");
+            CycleOne.TurnHead(direction);
+
+        }
+    }
+}
