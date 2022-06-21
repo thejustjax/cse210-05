4adc6d54723260d1ea738d49f468f2c501069ca2
Jax McKinney <mck20018@byui.edu>
2022-06-21 09:38:06 -0600

    fixed cycletwo

--- Game/Casting/CycleTwo.cs
+++ Game/Casting/CycleTwo.cs
@@ -1,49 +1,122 @@
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Unit05_cycle.Game.Casting
 {
     /// <summary>
-    /// <para>A tasty item that snakes like to eat.</para>
-    /// <para>
-    /// The responsibility of Food is to select a random position and points that it's worth.
-    /// </para>
+    /// <para>A long limbless reptile.</para>
+    /// <para>The responsibility of Snake is to move itself.</para>
     /// </summary>
-    public class Cycle_Two : Actor
+    public class CycleTwo : Actor
     {
-        private int points = 0;
+        private List<Actor> segments = new List<Actor>();
 
         /// <summary>
-        /// Constructs a new instance of an Food.
+        /// Constructs a new instance of a Snake.
         /// </summary>
-        public Food()
+        public CycleTwo()
         {
-            SetText("@");
-            SetColor(Constants.RED); 
-            Reset();
+            PrepareBody();
         }
 
         /// <summary>
-        /// Gets the points this food is worth.
+        /// Gets the snake's body segments.
         /// </summary>
-        /// <returns>The points.</returns>
-        public int GetPoints()
+        /// <returns>The body segments in a List.</returns>
+        public List<Actor> GetBody()
         {
-            return points;
+            return new List<Actor>(segments.Skip(1).ToArray());
         }
 
         /// <summary>
-        /// Selects a random position and points that the food is worth.
+        /// Gets the snake's head segment.
         /// </summary>
-        public void Reset()
+        /// <returns>The head segment as an instance of Actor.</returns>
+        public Actor GetHead()
         {
-            Random random = new Random();
-            points = random.Next(9);
-            int x = random.Next(Constants.COLUMNS);
-            int y = random.Next(Constants.ROWS);
-            Point position = new Point(x, y);
-            position = position.Scale(Constants.CELL_SIZE);
-            SetPosition(position);
+            return segments[0];
+        }
+
+        /// <summary>
+        /// Gets the snake's segments (including the head).
+        /// </summary>
+        /// <returns>A list of snake segments as instances of Actors.</returns>
+        public List<Actor> GetSegments()
+        {
+            return segments;
+        }
+
+        /// <summary>
+        /// Grows the snake's tail by the given number of segments.
+        /// </summary>
+        /// <param name="numberOfSegments">The number of segments to grow.</param>
+        public void GrowTail(int numberOfSegments)
+        {
+            for (int i = 0; i < numberOfSegments; i++)
+            {
+                Actor tail = segments.Last<Actor>();
+                Point velocity = tail.GetVelocity();
+                Point offset = velocity.Reverse();
+                Point position = tail.GetPosition().Add(offset);
+
+                Actor segment = new Actor();
+                segment.SetPosition(position);
+                segment.SetVelocity(velocity);
+                segment.SetText("#");
+                segment.SetColor(Constants.GREEN);
+                segments.Add(segment);
+            }
+        }
+
+        /// <inheritdoc/>
+        public override void MoveNext()
+        {
+            foreach (Actor segment in segments)
+            {
+                segment.MoveNext();
+            }
+
+            for (int i = segments.Count - 1; i > 0; i--)
+            {
+                Actor trailing = segments[i];
+                Actor previous = segments[i - 1];
+                Point velocity = previous.GetVelocity();
+                trailing.SetVelocity(velocity);
+            }
+        }
+
+        /// <summary>
+        /// Turns the head of the snake in the given direction.
+        /// </summary>
+        /// <param name="velocity">The given direction.</param>
+        public void TurnHead(Point direction)
+        {
+            segments[0].SetVelocity(direction);
+        }
+
+        /// <summary>
+        /// Prepares the snake body for moving.
+        /// </summary>
+        private void PrepareBody()
+        {
+            int x = Constants.MAX_X / 2;
+            int y = Constants.MAX_Y / 2;
+
+            for (int i = 0; i < Constants.SNAKE_LENGTH; i++)
+            {
+                Point position = new Point(x - i * Constants.CELL_SIZE, y);
+                Point velocity = new Point(1 * Constants.CELL_SIZE, 0);
+                string text = i == 0 ? "8" : "#";
+                Color color = i == 0 ? Constants.YELLOW : Constants.GREEN;
+
+                Actor segment = new Actor();
+                segment.SetPosition(position);
+                segment.SetVelocity(velocity);
+                segment.SetText(text);
+                segment.SetColor(color);
+                segments.Add(segment);
+            }
         }
     }
 }
